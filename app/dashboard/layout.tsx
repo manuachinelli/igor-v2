0541b17a979958a4d811a01c515b43616ba97cb1
manuachinelli <manuel.achinelli@brandtrack.fm>
2025-05-24 13:29:20 -0300

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,3 +1,3 @@
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return <>{children}</>
 }
